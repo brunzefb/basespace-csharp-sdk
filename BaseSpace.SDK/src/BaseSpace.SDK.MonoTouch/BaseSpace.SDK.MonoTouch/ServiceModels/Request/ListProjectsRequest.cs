5f9202d846e6a4f6ab61805c8ac525075f6f6f08
Friedrich Brunzema <friedrich.brunzema@sciex.com>
2017-05-23 11:56:28 -0400

    uncommited

--- BaseSpace.SDK/src/BaseSpace.SDK.MonoTouch/BaseSpace.SDK.MonoTouch/ServiceModels/Request/ListProjectsRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿using Illumina.BaseSpace.SDK.Types;
-
-namespace Illumina.BaseSpace.SDK.ServiceModels
-{
-    public class ListProjectsRequest : AbstractResourceListRequest<ProjectsSortByParameters>
-    {
-        public string Name { get; set; }
-    }
-}
