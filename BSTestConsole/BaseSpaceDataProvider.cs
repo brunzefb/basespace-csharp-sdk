8d463732b1bc8a73a07fd0593d85c7a74e5cc9dd
Friedrich Brunzema <friedrich.brunzema@sciex.com>
2017-05-23 14:39:44 -0400

    works if you replace token

---
+++ BSTestConsole/BaseSpaceDataProvider.cs
@@ -0,0 +1,46 @@
+﻿// Copyright SCIEX 2017. All rights reserved.
+
+using System;
+using Illumina.BaseSpace.SDK;
+using Illumina.BaseSpace.SDK.ServiceModels;
+using Illumina.BaseSpace.SDK.Types;
+
+
+namespace BSTestConsole
+{
+	public class BaseSpaceDataProvider 
+	{
+		private readonly BaseSpaceClient _baseSpaceClient;
+		private readonly BaseSpaceClientSettings _settings;
+		private readonly string _accessToken;
+
+		public BaseSpaceDataProvider(string accessToken)
+		{
+			_accessToken = accessToken;
+			_settings = new BaseSpaceClientSettings
+			{
+				Authentication = new OAuth2Authentication(accessToken),
+				RetryAttempts = 1,
+				TimeoutMin = 1
+			};
+			_baseSpaceClient = new BaseSpaceClient(_settings);
+		}
+
+
+		public ProjectCompact[] GetProjects()
+		{
+			ProjectCompact[] lpr;
+			try
+			{
+				 lpr = _baseSpaceClient.ListProjects(new ListProjectsRequest { Limit = 100 })
+					.Response.Items;
+			}
+			catch (Exception ex)
+			{
+				throw new Exception(
+					"Failed to connect to BaseSpace to download the necessary files.  Please try again later.", ex);
+			}
+			return lpr;
+		}
+	}
+}
---
+++ BSTestConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BSTestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			const string token = "NOTREAL";
+
+			var client = new BaseSpaceDataProvider(token);
+			var r = client.GetProjects();
+			System.Diagnostics.Debug.WriteLine("Done");
+			foreach (var item in r)
+				Console.WriteLine(item.Name);
+
+			Console.ReadLine();
+		}
+    }
+}
+
